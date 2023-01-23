79e2c035b316d456ba032e8da2234468e0f1eca7
Magistry <Magistrigusti@yandex.ru>
2023-01-23 08:49:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className = "app-wrapper">
+      <header className = "header">
+        <img src='https://images.static-collegedunia.com/public/college_data/images/company_image/1577428702logo.png'></img>
+      </header>
+      <nav className="nav">
+        <div>
+          Profile
+        </div>
+      </nav>
+      <div className = "content">
+        menu
+      </div>
+    </div>
+  );
+}
+
+
+
+
+export default App;
