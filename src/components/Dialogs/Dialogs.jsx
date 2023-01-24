57becf75bc5a4486d71970a605b5d15b9eb0dd78
Magistry <Magistrigusti@yandex.ru>
2023-01-25 01:09:50 +0200

    0.03

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,11 +3,43 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      <div className = {classes.dialogs}>
-        <div>
-        Dialogs
+    <div className = {classes.dialogs}>
+      <div className = {classes.dialogs_items}>
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/4202/4202832.png" alt="" />
+          <div className = {classes.name}>Maria</div>
         </div>
+
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/4202/4202839.png" alt="" />
+          <div className = {classes.name}>Andry</div>
+        </div>
+
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/4202/4202836.png" alt="" />
+          <div className = {classes.name}>Dmitrii</div>
+        </div>
+
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/9416/9416234.png" alt="" />
+          <div className = {classes.name}>Sasha</div>
+        </div>
+
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/4202/4202840.png" alt="" />
+          <div className = {classes.name}>Adi</div>
+        </div>
+
+        <div className = {classes.dialog}>
+          <img className = {classes.img} src="https://cdn-icons-png.flaticon.com/512/4202/4202831.png" alt=""/>
+          <div className = {classes.name}>Ivan</div>
+        </div>
+      </div>
+
+      <div className = {classes.messages}>
+        <div className = {classes.message}>Hey</div>
+        <div className = {classes.message}>How are you?</div>
+        <div className = {classes.message}>My name is Dima</div>
       </div>
     </div>
   );
