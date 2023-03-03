62442c59f8e5a48ffb7cbf6b5f5ddfda7f38f6bb
Magistry <Magistrigusti@yandex.ru>
2023-03-03 02:51:56 +0200

    0.28

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,5 +1,8 @@
+import { v1 } from 'uuid';
+
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
+const avatarLink = "https://cdn-icons-png.flaticon.com/512/4202/4202836.png";
 
 let initialState = {
     dialogsData: [
@@ -8,7 +11,7 @@ let initialState = {
         { id: 3, name: 'Dmitrii', src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png" },
         { id: 4, name: 'Sasha', src: "https://cdn-icons-png.flaticon.com/512/9416/9416234.png" },
         { id: 5, name: 'Adi', src: "https://cdn-icons-png.flaticon.com/512/4202/4202840.png" },
-        { id: 6, name: 'Ivan', src: "https://cdn-icons-png.flaticon.com/512/4202/4202840.png" }
+        { id: 6, name: 'Ivan', src: "https://cdn-icons-png.flaticon.com/512/163/163801.png" }
     ],
 
     messagesData: [
@@ -21,29 +24,27 @@ let initialState = {
 }
 
 const dialogsReducer = (state = initialState, action) => {
-    let stateCopy;
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            stateCopy = {
+            return {
                 ...state,
                 newMessageBody: action.body
-            };
-            return stateCopy;
-        case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            stateCopy = {
-                ...state,
-                newMessageBody: '',
-                messagesData: [...state.messagesData, { id: 9, text: body, src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png" }]
-            };
-            return stateCopy
-        default:
-            return state;
+            }
+            case SEND_MESSAGE:
+                let body = state.newMessageBody;
+                return {
+                    ...state,
+                    newMessageBody: '',
+                    messagesData: [...state.messagesData, {id: v1(), text: body, src: avatarLink}]
+                }
+            default:
+                return state;
     }
 }
 
-export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
-export const updateNewMessageBodyCreator = (text) =>
-    ({ type: UPDATE_NEW_MESSAGE_BODY, body: text });
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (text) => 
+    ({type: UPDATE_NEW_MESSAGE_BODY, body: text});
 
 export default dialogsReducer;
+
--- src/redux/try.js
+++ src/redux/try.js
@@ -1,143 +1,47 @@
-const ADD_POST = "ADD-POST";
-const ADD_MESSAGE = "ADD-MESSAGE";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
-const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
+const avatarLink = "https://cdn-icons-png.flaticon.com/512/4202/4202836.png";
 
-let store = {
-  _state: {
-    profilePage: {
-      postsData: [
-        { id: 1, message: 'Hi, how are you?', likesCount: '28', dislikesCount: 2 },
-        { id: 2, message: 'It is a social network on the blockhain', likesCount: 22, dislikesCount: 1 },
-        { id: 3, message: 'Are you ready', likesCount: 30, dislikesCount: 0 }
-      ],
-      newPostText: 'TonCoin',
-    },
-
-    dialogsPage: {
-      dialogsData: [
+let initialState = {
+    dialogsData: [
         { id: 1, name: 'Maria', src: "https://cdn-icons-png.flaticon.com/512/4202/4202832.png" },
         { id: 2, name: 'Andry', src: "https://cdn-icons-png.flaticon.com/512/4202/4202839.png" },
         { id: 3, name: 'Dmitrii', src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png" },
         { id: 4, name: 'Sasha', src: "https://cdn-icons-png.flaticon.com/512/9416/9416234.png" },
         { id: 5, name: 'Adi', src: "https://cdn-icons-png.flaticon.com/512/4202/4202840.png" },
         { id: 6, name: 'Ivan', src: "https://cdn-icons-png.flaticon.com/512/4202/4202840.png" }
-      ],
+    ],
 
-      messagesData: [
+    messagesData: [
         { id: 1, text: 'Hey', src: "https://cdn-icons-png.flaticon.com/512/4202/4202839.png" },
         { id: 2, text: 'Hello Andry', src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png" },
         { id: 3, text: 'How are you Dmittrii', src: "https://cdn-icons-png.flaticon.com/512/4202/4202839.png" },
         { id: 4, text: 'i,m fune, and you?', src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png" }
-      ],
-      newMessageText: 'TonCoin',
-    },
+    ],
+    newMessageBody: '',
+}
 
-    newsPage: {
-      newsData: [
-        { id: 1, title: 'Cripto News', text: 'Итоги недели: Binance обвинили в обработке транзакций Bitzlato, а у Бэнкмана-Фрида конфисковали почти $700 млн', src: "https://cnews24.ru/uploads/08f/08f8f84ab0ddc6016e441b2ea24896b70e453cab.webp" },
-        { id: 2, title: 'Cripto News', text: 'XRP Price Prediction – Can XRP Go to $5 This Year?', src: 'https://v2.cimg.co/news/102948/253724/responsive-images/xrp___media_library_original_1735_956.jpg' }
-      ],
+const dialogsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+          return {
+            ...state,
+            newMessageBody: action.body
+          }
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            return {
+              ...state,
+              newMessageBody: '',
+              messagesData: [...state.messagesData, {id: 9, text: body, src: avatarLink}]
+            }
+        default:
+            return state;
     }
-
-  },
-  _callSubscriber() {
-    console.log('State changed');
-  },
-  getState() {
-    return this._state;
-  },
-  sunscribe(observer) {
-  this._callSubscriber = observer;
-},
-addPost() {
-  let newPost = {
-    id: 4,
-    message: this._state.profilePage.newPostText,
-    lekesCount: 4,
-    dislikesCount: 0
-  };
-
-  this._state.profilePage.postsData.push(newPost);
-  this._state.profilePage.newPostText = '';
-  this._callSubscriber(this._state);
-},
-addMessage() {
-  let newMessage = {
-    id: 8,
-    text: this._state.dialogsPage.newMessageText,
-    src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png"
-  };
-
-  this._state.dialogsPage.messagesData.push(newMessage);
-  this._state.dialogsPage.newMessageText = '';
-  this._callSubscriber(this._state);
-},
-updateNewPostText(newText) {
-  this._state.profilePage.newPostText = newText;
-  this._callSubscriber();
-},
-updateNewDialogsText(newText) {
-  this._state.dialogsPage.newMessageText = newText;
-  this._callSubscriber(this._state)
-},
-dispatch(action) {
-  if (action.type === 'ADD-POST') {
-    let newPost = {
-      id: 4,
-      message: this._state.profilePage.newPostText,
-      likesCount: 4,
-      dislikesCount: 0
-    };
-    this._state.profilePage.postsData.push(newPost);
-    this._state.profilePage.newPostText = '';
-    this._callSubscriber(this._state);
-  } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-    this._state.profilePage.newPostText = action.newText;
-    this._callSubscriber(this._state);
-  }
-
-  if (action.type === 'ADD-MESSAGE') {
-    let newMessage ={
-      id: 9,
-      message: this._state.dialogsPage.newMessageText,
-      src: "https://cdn-icons-png.flaticon.com/512/4202/4202836.png"
-    };
-    this._state.dialogsPage.messagesData.push(newMessage);
-    this._state.dialogsPage.newMessageText = '';
-    this._callSubscriber(this._state);
-  } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
-    this._state.dialogsPage.newMessageText = action.newText;
-    this._callSubscriber(this._state);
-  }
-},
 }
 
-export const addPostActionCreator = () => {
-  return {
-    type: ADD_POST
-  }
-}
-export const updateNewPostTextActionCreator = (text) => {
-  return {
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text
-  }
-}
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
+export const updateNewMessageBodyCreator = (text) =>
+    ({ type: UPDATE_NEW_MESSAGE_BODY, body: text });
 
-export const addMessageActionCreator = () => {
-  return {
-    type: ADD_MESSAGE
-  }
-}
-export const updateNewMessageTextActionCreator = (text) => {
-  return {
-    type: UPDATE_NEW_MESSAGE_TEXT,
-    newText: text
-  }
-}
-
-
-window.store = store;
-
-export default store;
+export default dialogsReducer;
