47e3581fd27581c554f77bc71d638ee5620f7925
Magistry <Magistrigusti@yandex.ru>
2023-03-04 15:15:44 +0200

    0.31

---
+++ src/redux/users-reducer.js
@@ -0,0 +1,13 @@
+
+let initialState = {
+
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
