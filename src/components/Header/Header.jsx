b2878a179bd69fa81299677883fea072e5dd09dd
Magistry <Magistrigusti@yandex.ru>
2023-04-10 15:17:38 +0300

    0.35

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ import classes from './Header.module.css';
 const Header = () => {
   return (
     <header className = {classes.header}>
-        <img className = {classes.header_img} src='https://images.static-collegedunia.com/public/college_data/images/company_image/1577428702logo.png' alt=""></img>
+        <div className = {classes.img_wrapper}>
+          <img className = {classes.header_img} src='https://www.ph4.ru/DL/LOGO/t/tonproject.gif' alt=""></img>
+        </div>
         <div className = {classes.header_title}>
           <h2 className = {classes.title}>Ton-So-Net</h2>
         </div>
