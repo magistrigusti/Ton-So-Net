4eb7832198826938e33082b35dcf07e42ee11a0a
Magistry <Magistrigusti@yandex.ru>
2023-05-22 12:35:32 +0300

    0.38

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,28 +4,31 @@ import classes from './Users.module.css';
 import userPhoto from '../../assets/images/user-image.jpg';
 
 let Users = (props) => {
-
-  if (props.users.length === 0) {
-    const apiUrl = 'https://social-network.samuraijs.com/api/1.0/users';
-    axios.get(apiUrl).then(response => {
-      props.setUsers(response.data.items);
-    });
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      const apiUrl = 'https://social-network.samuraijs.com/api/1.0/users';
+      axios.get(apiUrl).then(response => {
+        props.setUsers(response.data.items);
+      });
+    }
   }
-  
+
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {
         props.users.map(user =>
           <div className={classes.user_card} key={user.id}>
             <span>
               <div className={classes.avatar}>
-                <img className={classes.avatar_img} src={user.photos.small !=null ? user.photos.small : userPhoto } alt=''/>
+                <img className={classes.avatar_img} src={user.photos.small != null ? user.photos.small : userPhoto} alt='' />
               </div>
               <div className={classes.btn_wrapper}>
                 {user.followed ?
                   <button className={classes.btn} onClick={() => { props.unfollow(user.id) }} >Unfollow</button>
                   :
-                  <button className={classes.btn} onClick={() => { props.follow(user.id) }} >Follow</button>}
+                  <button className={classes.btn} onClick={() => { props.follow(user.id) }} >Follow</button>
+                }
               </div>
             </span>
             <span className={classes.user_name}>
